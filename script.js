e5415fc37a814c67cd701c17a3ecbf76b2e24af9
Jeferson Chagas <jeferson_chagas25@hotmail.com>
2022-08-12 15:09:27 -0300

    Adicionando informações nas pizzas, imagem, preço, nome e descrição.

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ const qa = (element) => document.querySelectorAll(element);
 pizzaJson.map((item, indice) => {
     let pizzaItem = q('.models .pizza-item').cloneNode(true);
 
+    
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
     q('.pizza-area').append(pizzaItem);
 });
